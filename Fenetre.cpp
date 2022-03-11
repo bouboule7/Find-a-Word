22ccd7dc2577236591d3f65e558950301f9791d1
Jeanny Fidelica <93310908+bouboule7@users.noreply.github.com>
2022-03-11 07:22:58 +0300

    Add files via upload

--- Fenetre.cpp
+++ Fenetre.cpp
@@ -1,68 +1,99 @@
-#include "Fenetre.hpp"                                  //car on va utiliser la classe fenetre
+#include "Fenetre.hpp"                                          //car on va utiliser la classe fenetre
 
-Fenetre::Fenetre() {                                    //constructeur
-                                                        //Modification de la fenêtre.
-    set_title("Find a word");                           //pour editer le titre de la fenetre
-    resize(500, 350);                                   //on redimensionne la fenetre au taille 640 * 480
+Fenetre::Fenetre() {                                            //constructeur
+                                                                //Modification de la fenêtre.
+    set_title("Find a word");                                   //pour editer le titre de la fenetre
+    resize(500, 350);                                           //on redimensionne la fenetre au taille 640 * 480
 
-    set_position(Gtk::WIN_POS_CENTER);                  //on positionne la fenetre au centre
+    set_position(Gtk::WIN_POS_CENTER);                          //on positionne la fenetre au centre
+
     
-    add(boiteH);                                        //addition de boiteH boite horizontale au fenetre qui va tout contenir
-    boiteH.pack_start(boiteV,Gtk::PACK_SHRINK);         //on ajoute boiteV dans boiteH              ///PACK_SHRINK sert a ajuster l'espacement des widgets
-    boiteH.pack_end(boiteBouttonH);                     //on ajoute la boite contenant les deux bouttons dans boiteH
-    boiteBouttonH.pack_start(nouveau,Gtk::PACK_SHRINK); //on ajoute dans la boite de boutton le boutton nouveau
-    boiteBouttonH.pack_start(load,Gtk::PACK_SHRINK);
-    boiteBouttonH.pack_start(nouveau,Gtk::PACK_SHRINK); //on ajoute dans la boite de boutton le boutton load
-    
-    boiteV.pack_start(barreMenu, Gtk::PACK_SHRINK);     //on ajoute la barre de Menu dans la boiteV
-    menuItemMenu.set_label("MENU");                     //on ajoute le text "MENU" du menuItemMenu
-    barreMenu.append(menuItemMenu);                     //on ajoute des sous menu a menuItemMenu
+    add(boiteH);                                                //addition de boiteH boite horizontale au fenetre qui va tout contenir
+    boiteH.pack_start(boiteV,Gtk::PACK_SHRINK);                 //on ajoute boiteV dans boiteH              ///PACK_SHRINK sert a ajuster l'espacement des widgets
+        boiteV.pack_start(barreMenu, Gtk::PACK_SHRINK);         //on ajoute la barre de Menu dans la boiteV
+            menuItemMenu.set_label("MENU");                     //on ajoute le text "MENU" du menuItemMenu
+            barreMenu.append(menuItemMenu);                     //on ajoute des sous menu a menuItemMenu
 
-    menuItemMenu.set_submenu(menuMenu);                 //on donne le sub menuNouveau comme submenu
-    menuNouveau.set_label("nouveau");                   //on donne le text pour menuNouveau "nouveau"
-    menuMenu.append(menuNouveau);                       //on fixe menuNoouveau comme submenu
-    menuEnregistrer.set_label("Enregistrer");           //on donne le text pour menuNouveau "nouveau"
-    menuMenu.append(menuEnregistrer);                   //on ajoute menuEnregistrer au submenu
-    menuQuitter.set_label("QUITTER !");                 //on donne le text pour le surmenu quitter "QUITTER"     
-    menuMenu.append(menuQuitter);                       //on ajoute le sub menu quitter
-    
-    menuItemAide.set_label("Aide");                     //on donne le text pour le second menu "aide"
-    barreMenu.append(menuItemAide);                     //on ajouter le menu aide au barre de menu
+            menuItemMenu.set_submenu(menuMenu);                 //on donne le sub menuNouveau comme submenu
+                menuNouveau.set_label("nouveau");               //on donne le text pour menuNouveau "nouveau"
+                menuMenu.append(menuNouveau);                   //on fixe menuNoouveau comme submenu
+                menuEnregistrer.set_label("Enregistrer");       //on donne le text pour menuNouveau "nouveau"
+                menuMenu.append(menuEnregistrer);               //on ajoute menuEnregistrer au submenu
+                menuQuitter.set_label("QUITTER !");             //on donne le text pour le surmenu quitter "QUITTER"     
+                menuMenu.append(menuQuitter);                   //on ajoute le sub menu quitter
+                
+            menuItemAide.set_label("Aide");                     //on donne le text pour le second menu "aide"
+            barreMenu.append(menuItemAide);                     //on ajouter le menu aide au barre de menu
 
-    menuItemAide.set_submenu(menuAide);                 //on va ajouter des sub menu au menu aide
-    menuText.set_label("Jeu du Find a word. \n Le but du jeu est de trouver un mot contenant \nles lettres proposer alléatoirement par le jeu.");
-                                                        //on donne le text contenant l'aide
-    menuAide.append(menuText);                          //on ajouter la section contenant l'aide au submenu de facon a ce que quand on se focalise sur le menu aide, le long text appareit
+            menuItemAide.set_submenu(menuAide);                 //on va ajouter des sub menu au menu aide
+            menuText.set_label("Jeu du Find a word. \n Le but du jeu est de trouver un mot contenant \nles lettres proposer alléatoirement par le jeu.");
+                                                                //on donne le text contenant l'aide
+                menuAide.append(menuText);                      //on ajouter la section contenant l'aide au submenu de facon a ce que quand on se focalise sur le menu aide, le long text appareit
 
-                                                        //les bouttons principale au demarage
-    nouveau.set_label("Nouveau");                       //on donne le text pour le boutton nouveau
-    load.set_label("Load");                             //on donne le text pour le boutton loads
-    
-    boiteV.show();                                      //on affiche le boiteV                 boite pricipale
-        boiteH.show();                                  //on affiche le boiteH                 contenant le menu en haut
-            barreMenu.show();                           //on affiche barreMenu 
-                menuItemMenu.show();                    //on affiche menuItemMenu    
-                    menuMenu.show();                    //on affiche menuMenu
-                    menuNouveau.show();                 //on affiche menuNouveau
-                    menuEnregistrer.show();             //on affiche menuEnregistrer
-                    menuQuitter.show();                 //on affiche menuQuitter
-                menuItemAide.show();                    //on affiche menuItemAide
-                    menuAide.show();                    //on affiche menuAide
-                    menuText.show();                    //on affiche menuText
-        boiteBouttonH.show();                           //on affiche boiteBouttonH            contenant tout le corps
-            nouveau.show();                             //on affiche le boutton nouveau
-            load.show();                                //on affiche le boutton load
+    boiteH.pack_start(boiteBouttonH);                             //on ajoute la boite contenant les deux bouttons dans boiteH
+                                                                //les bouttons principale au demarage
+        boiteBouttonH.pack_start(nouveau,Gtk::PACK_SHRINK);     //on ajoute dans la boite de boutton le boutton nouveau
+            nouveau.set_label("Nouveau");                       //on donne le text pour le boutton nouveau
+        boiteBouttonH.pack_start(load,Gtk::PACK_SHRINK);        //on ajoute dans la boite de boutton le boutton load
+            load.set_label("Load");                             //on donne le text pour le boutton loads
+        boiteBouttonH.pack_end(level1,Gtk::PACK_SHRINK);        //on ajoute dans la boite de boutton le boutton level1
+            level1.set_label(" 1 ");                             //on donne le text pour le boutton level1
+        boiteBouttonH.pack_end(level2,Gtk::PACK_SHRINK);        //on ajoute dans la boite de boutton le boutton lelev2
+            level2.set_label(" 2 ");                             //on donne le text pour le boutton level2
+        boiteBouttonH.pack_end(level3,Gtk::PACK_SHRINK);        //on ajoute dans la boite de boutton le boutton level3
+            level3.set_label(" 3 ");                             //on donne le text pour le boutton level3
+        boiteBouttonH.pack_end(level4,Gtk::PACK_SHRINK);        //on ajoute dans la boite de boutton le boutton level4
+            level4.set_label(" 4 ");                             //on donne le text pour le boutton level4
+        boiteBouttonH.pack_end(level5,Gtk::PACK_SHRINK);        //on ajoute dans la boite de boutton le boutton level5s
+            level5.set_label(" 5 ");                             //on donne le text pour le boutton level5
+    boiteH.pack_start(textReponse);
+
+    boiteV.show();                                              //on affiche le boiteV                 boite pricipale
+        boiteH.show();                                          //on affiche le boiteH                 contenant le menu en haut
+            barreMenu.show();                                   //on affiche barreMenu 
+                menuItemMenu.show();                            //on affiche menuItemMenu    
+                    menuMenu.show();                            //on affiche menuMenu
+                    menuNouveau.show();                         //on affiche menuNouveau
+                    menuEnregistrer.show();                     //on affiche menuEnregistrer
+                    menuQuitter.show();                         //on affiche menuQuitter
+                menuItemAide.show();                            //on affiche menuItemAide
+                    menuAide.show();                            //on affiche menuAide
+                    menuText.show();                            //on affiche menuText
+        boiteBouttonH.show();                                   //on affiche boiteBouttonH            contenant tout le corps
+            nouveau.show();                                     //on affiche le boutton nouveau
+            load.show();                                        //on affiche le boutton load
+        textReponse.show();
 
     //on change d'ecran cad appele du fonction changerEcran
-    nouveau.signal_clicked().connect(sigc::mem_fun(*this, &Fenetre::changerEcran));
-    load.signal_clicked().connect(sigc::mem_fun(*this, &Fenetre::changerEcran));
+    nouveau.signal_clicked().connect(sigc::mem_fun(*this, &Fenetre::afficheChoixLevel));
+    load.signal_clicked().connect(sigc::mem_fun(*this, &Fenetre::cacherBouttonNewEtLoad));
 
+    //edition de l'action menuEnregistrer
+    menuEnregistrer.signal_activate().connect(sigc::mem_fun(*this, &Fenetre::sauvegarde));
     //edition de l'action menuQuitter
     menuQuitter.signal_activate().connect(sigc::ptr_fun(&Gtk::Main::quit));
 }
 
-void Fenetre::changerEcran(){                           //definition du fonction changerEcran
-    load.hide();                                        //on cache le boutton load
-    nouveau.hide();                                     //on cache le boutton nouveau
+void Fenetre::cacherBouttonNewEtLoad(){                         //definition du fonction changerEcran
+    load.hide();                                                //on cache le boutton load
+    nouveau.hide();                                             //on cache le boutton nouveau
 }
-
+void Fenetre::afficheChoixLevel(){
+    cacherBouttonNewEtLoad();
+    level1.show();
+    level2.show();
+    level3.show();
+    level4.show();
+    level5.show();
+}
+void Fenetre::sauvegarde(){
+    Gtk::MessageDialog dialogue(*this,"",true);
+    dialogue.set_title("Sauvegarde");
+    dialogue.set_secondary_text("Votre progression et vos donné ont été enregistrer avec <b>SUCCESS</b>",true);
+    dialogue.run();
+}
+/*
+void Fenetre::afficheTextEdit();
+void Fenetre::afficheLettre();
+void Fenetre::afficheZoneDeTextRepose();
+*/
--- Fenetre.hpp
+++ Fenetre.hpp
@@ -4,14 +4,15 @@
 #include <gtkmm/window.h>                                   //car on va herité de fenetre
 #include <gtkmm/box.h>                                      //modele de boite
 #include <gtkmm/buttonbox.h>                                //modele de boite pour les bouttons
-#include <gtkmm/imagemenuitem.h>
-#include <gtkmm/main.h>
-#include <gtkmm/menu.h>
-#include <gtkmm/menubar.h>
-#include <gtkmm/menuitem.h>
-#include <gtkmm/separatormenuitem.h>
-#include <gtkmm/stock.h>
-#include <gtkmm/button.h>
+#include <gtkmm/imagemenuitem.h>                            //pour les sous menu
+#include <gtkmm/main.h>                                     //pour la fonction principale de gtkmm
+#include <gtkmm/menu.h>                                     //pour l'ajout des menus
+#include <gtkmm/menubar.h>                                  //utilisation du barre de menu
+#include <gtkmm/menuitem.h>                                 //gestion des sous menu
+#include <gtkmm/stock.h>                                    //pour utiliser les Stock Items
+#include <gtkmm/button.h>                                   //utilisation des boutton
+#include <gtkmm/messagedialog.h>                            //boite de dialogue
+#include <gtkmm/entry.h>
 
 #include "Boutton.hpp"                                      //appele de notre objet boutton personnalisé
 
@@ -19,7 +20,12 @@ class Fenetre : public Gtk::Window {
     public :
         Fenetre();                                          //constructeur
 
-        void changerEcran();
+        void cacherBouttonNewEtLoad();
+        void sauvegarde();
+        void afficheChoixLevel();
+        void afficheTextEdit();
+        void afficheLettre();
+        void afficheZoneDeTextRepose();
     private:
         Gtk::VBox boiteV;    
             Gtk::HBox boiteH;                               //ici commence les items du menubar
@@ -33,11 +39,17 @@ class Fenetre : public Gtk::Window {
                         Gtk::MenuItem menuItemAide;         //Menu Aide
                         Gtk::ImageMenuItem menuText;        //le long text contenant l aide est placé sous forme de menu
                                                             //ici se termine les items du menubarre
+                        Gtk::Entry textReponse;
+                        
 
         //ici commence les items dans le corps
         Gtk::HButtonBox boiteBouttonH;
             Gtk::Button nouveau;
             Gtk::Button load;
+                    Gtk::Button level1;                 //boutton pour le niveau 1
+                    Gtk::Button level2;                 //boutton pour le niveau 2
+                    Gtk::Button level3;                 //boutton pour le niveau 3
+                    Gtk::Button level4;                 //boutton pour le niveau 4                        Gtk::Button level5;                 //boutton pour le niveau 5
 
 };
 
