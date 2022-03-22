8458abc0090733982234ea2c9d300833af64caec
Jeanny Fidelica <93310908+bouboule7@users.noreply.github.com>
2022-03-22 11:41:32 +0300

    Update Donnee.hpp

--- Donnee.hpp
+++ Donnee.hpp
@@ -1,14 +1,22 @@
 #ifndef DONNEE_HPP
 #define DONNEE_HPP
 #include <fstream>              //utilisation des fichier
+#include <vector>
+using namespace std;
 
 class Donnee{
     private:
         char debutDeMot;
         int score;
-
+        int niveau;
+        fstream fichierProgression;
+        ifstream motFrancais;
+        string mot;
     public:
         Donnee();
-        ~Donnee();
+        void changerDeNiveau(int niveauUtilisateur);
+        int getNiveau();
+        bool rechercheResultatDansListeDeMot(vector <string> lettreAlleatoire, string motUtilisateur);
+        string nomDuFichier(string lettre);
 };
 #endif
